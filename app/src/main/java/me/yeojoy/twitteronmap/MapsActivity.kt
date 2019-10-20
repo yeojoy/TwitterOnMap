5a92bd1d29c11f6c2b97d2dae1ebccebeb9ea0f6
yeojoy <yeojoy.kim@gmail.com>
2019-10-20 17:30:23 -0400

    add bottomsheet layout
    able to control that widget.

--- app/src/main/java/me/yeojoy/twitteronmap/MapsActivity.kt
+++ app/src/main/java/me/yeojoy/twitteronmap/MapsActivity.kt
@@ -7,7 +7,10 @@ import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
@@ -19,9 +22,11 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_maps.*
 import kotlinx.android.synthetic.main.activity_twitter.*
+import kotlinx.android.synthetic.main.layout_bottom_sheet.*
 
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -34,14 +39,15 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var locationRequest: LocationRequest
     private lateinit var locationCallback: MapLocationCallback
+    private lateinit var sheetBehavior: BottomSheetBehavior<ConstraintLayout>
+
     private var lastLatLng: LatLng? = null
 
     private val runnable = Runnable {
         mMap?.let {
             Log.d(TAG, "run()")
-            val latLng = LatLng(
-                mMap.cameraPosition.target.latitude, mMap.cameraPosition.target.longitude
-            )
+            val latLng = LatLng(mMap.cameraPosition.target.latitude,
+                    mMap.cameraPosition.target.longitude)
             addMarker(latLng)
 
             // TODO refresh tweets
@@ -53,7 +59,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment =
-            supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
+                supportFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
         if (!checkPermission()) {
@@ -61,54 +67,37 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
 
         initializeLocation()
-    }
 
-    private fun initializeLocation() {
-        fusedLocationClient = FusedLocationProviderClient(this)
+        sheetBehavior = BottomSheetBehavior.from<ConstraintLayout>(bottomSheet)
+        sheetBehavior.setBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slidingOffset: Float) {
 
-        locationRequest = LocationRequest()
-        locationRequest.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
-        locationRequest.interval = 3 * 60 * 1000
-        locationRequest.fastestInterval = 1 * 60 * 1000
+            }
 
-        locationCallback = MapLocationCallback()
-    }
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_HIDDEN -> {
+                        textViewState.text = "BottomSheetBehavior.STATE_HIDDEN"
+                    }
+                    BottomSheetBehavior.STATE_EXPANDED -> {
+                        textViewState.text = "BottomSheetBehavior.STATE_EXPANDED"
 
+                    }
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+                        textViewState.text = "BottomSheetBehavior.STATE_COLLAPSED"
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+                    }
+                    BottomSheetBehavior.STATE_DRAGGING -> {
+                        textViewState.text = "BottomSheetBehavior.STATE_DRAGGING"
 
-        mMap.setOnCameraMoveListener {
-            val latLng = mMap.cameraPosition.target
-            Log.d(
-                TAG,
-                "setOnCameraMoveListener : Latitude ${latLng.latitude}, Longitude ${latLng.longitude}"
-            )
+                    }
+                    BottomSheetBehavior.STATE_SETTLING -> {
+                        textViewState.text = "BottomSheetBehavior.STATE_SETTLING"
 
-            sendLocation()
-        }
-
-        // default location is Mont royal observatory
-        // 45.5039267,-73.5881409
-        val montRoyalObservatory = LatLng(45.5039267, -73.5881409)
-
-        moveToLocation(montRoyalObservatory)
-    }
-
-    private fun sendLocation() {
-        Log.i(TAG, "sendLocation()")
-
-        layout_map.removeCallbacks(runnable)
-        layout_map.postDelayed(runnable, REFRESH_TIMER)
+                    }
+                }
+            }
+        })
     }
 
     override fun onResume() {
@@ -124,6 +113,52 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         removeLocationListener()
     }
 
+
+    private fun initializeLocation() {
+        fusedLocationClient = FusedLocationProviderClient(this)
+
+        locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+        locationRequest.interval = 3 * 60 * 1000
+        locationRequest.fastestInterval = 1 * 60 * 1000
+
+        locationCallback = MapLocationCallback()
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        mMap.setOnCameraMoveListener {
+            val latLng = mMap.cameraPosition.target
+            Log.d(TAG,
+                    "setOnCameraMoveListener : Latitude ${latLng.latitude}, Longitude ${latLng.longitude}")
+
+            sendLocation()
+        }
+
+        // default location is Mont royal observatory
+        // 45.5039267,-73.5881409
+        val montRoyalObservatory = LatLng(45.5039267, -73.5881409)
+
+        moveToLocation(montRoyalObservatory)
+    }
+
+    private fun sendLocation() {
+        Log.i(TAG, "sendLocation()")
+
+        layoutMap.removeCallbacks(runnable)
+        layoutMap.postDelayed(runnable, REFRESH_TIMER)
+    }
+
     private fun addLocationListener() {
         fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
     }
@@ -136,49 +171,43 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap?.let {
             mMap.clear()
             mMap.addMarker(MarkerOptions().position(latLng).title("your location"))
+            mMap.setOnMarkerClickListener {
+                sheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+                true
+            }
         }
     }
 
     private fun checkPermission(): Boolean {
-        return PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
-            this, Manifest.permission.ACCESS_FINE_LOCATION
-        )
+        return PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
     }
 
     private fun requestPersmission() {
-        var shouldProvideRationale = ActivityCompat.shouldShowRequestPermissionRationale(
-            this, Manifest.permission.ACCESS_FINE_LOCATION
-        )
+        var shouldProvideRationale = ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
 
         if (shouldProvideRationale) {
             Log.i(TAG, "Displaying permission rationale to provide additional context.")
-            Snackbar.make(
-                findViewById(R.id.layout_map),
-                R.string.permission_rationale,
-                Snackbar.LENGTH_INDEFINITE
-            ).setAction(R.string.ok) {
+            Snackbar.make(findViewById(R.id.layoutMap),
+                    R.string.permission_rationale,
+                    Snackbar.LENGTH_INDEFINITE).setAction(R.string.ok) {
                 // Request permission
-                ActivityCompat.requestPermissions(
-                    this@MapsActivity,
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                    REQUEST_LOCATION_PERMISSON_CODE
-                )
+                ActivityCompat.requestPermissions(this@MapsActivity,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        REQUEST_LOCATION_PERMISSON_CODE)
             }.show()
         } else {
             Log.i(TAG, "Requesting permission");
-            ActivityCompat.requestPermissions(
-                this@MapsActivity,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                REQUEST_LOCATION_PERMISSON_CODE
-            )
+            ActivityCompat.requestPermissions(this@MapsActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    REQUEST_LOCATION_PERMISSON_CODE)
         }
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<out String>,
+                                            grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         Log.i(TAG, "onRequestPermissionResult")
         if (requestCode == REQUEST_LOCATION_PERMISSON_CODE) {
@@ -188,17 +217,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 Log.i(TAG, "User interaction was cancelled.")
             } else {
                 // Permission denied.
-                Snackbar.make(
-                    findViewById(R.id.layout_map),
-                    R.string.permission_denied_explanation,
-                    Snackbar.LENGTH_INDEFINITE
-                ).setAction(R.string.settings) {
+                Snackbar.make(findViewById(R.id.layoutMap),
+                        R.string.permission_denied_explanation,
+                        Snackbar.LENGTH_INDEFINITE).setAction(R.string.settings) {
                     // Build intent that displays the App settings screen.
                     val intent = Intent()
                     intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
-                    val uri = Uri.fromParts(
-                        "package", BuildConfig.APPLICATION_ID, null
-                    )
+                    val uri = Uri.fromParts("package", BuildConfig.APPLICATION_ID, null)
                     intent.data = uri
                     intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
                     startActivity(intent)
