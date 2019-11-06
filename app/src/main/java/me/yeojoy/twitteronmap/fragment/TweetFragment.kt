da1bd572a23af0b20ac3e812b680a4c180d8d83d
yeojoy <yeojoy.kim@gmail.com>
2019-11-06 15:14:18 -0500

    add fragment and layout
    remove TODO, that is because that todo was done

--- app/src/main/java/me/yeojoy/twitteronmap/fragment/TweetFragment.kt
+++ app/src/main/java/me/yeojoy/twitteronmap/fragment/TweetFragment.kt
@@ -1,16 +1,45 @@
 package me.yeojoy.twitteronmap.fragment
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.gson.Gson
+import me.yeojoy.twitteronmap.network.model.Tweet
 
 class TweetFragment : Fragment() {
 
     companion object {
         private val TAG = TweetFragment::class.java.simpleName
+        private val KEY_TWEET = "tweet_"
 
-        fun newInstance() : TweetFragment {
+        fun newInstance(tweet: Tweet) : TweetFragment {
             // If it needs some arguments, set up here!
             val fragment = TweetFragment()
+            val bundle = Bundle()
+            bundle.putParcelable(KEY_TWEET, tweet)
+            fragment.arguments = bundle
             return fragment
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // TODO make layout for this fragment
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val tweet = arguments!!.getParcelable<Tweet>(KEY_TWEET)
+        tweet.let {
+            Log.d(TAG, Gson().toJson(tweet))
+
+
         }
     }
 
